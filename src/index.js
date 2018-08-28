ef21ffd6c440717944ae6606b783625e20d08701
ridoansaleh <ridoansalehnasution98@gmail.com>
2018-08-28 10:40:31 +0700

    add all files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+registerServiceWorker();
